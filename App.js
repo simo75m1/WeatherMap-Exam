b57b944420ca63c73ec99daab6e13814ed595ca8
simo75m1 <simo75m1@stud.kea.dk>
2024-05-07 15:12:42 +0200

    setup mapview

--- App.js
+++ App.js
@@ -1,18 +1,110 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect, useRef } from 'react';
+import { Image, StyleSheet, Text, View, Button, TextInput, Alert, FlatList, TouchableOpacity, Modal } from 'react-native';
+import MapView, {Marker} from 'react-native-maps'
+import * as Location from 'expo-location'
+
 
 export default function App() {
+
+  const [modalVisible, setModalVisible] = useState(false);
+  const [region, setRegion] = useState({
+    latitude:40,
+    longitude:-73,
+    latitudeDelta:20, //Hvor mange længde og breddegradder skal vises på kortet
+    longitudeDelta:20 //Mindre tal = mere zoomet ind. 
+  })
+
+  const mapView = useRef(null) // useRef minder om useState, men forårsager ikke en re-render af siden.
+  const locationSubscription = useRef(null)
+
+  useEffect(() => {
+    // Starte en listener
+    async function startListener(){
+      let {status} = await Location.requestForegroundPermissionsAsync()
+      if(status !== 'granted'){
+        alert("Fik ikke adgang til lokation")
+        return
+      }
+      locationSubscription.current = await Location.watchPositionAsync({
+        distanceInterval: 100,
+        accuracy: Location.Accuracy.High //den højeste præcision
+      }, (lokation) => {
+          const newRegion = {
+          latitude: lokation.coords.latitude,
+          longitude: lokation.coords.longitude,
+          latitudeDelta: 20,
+          longitudeDelta: 20
+          }
+          setRegion(newRegion)
+          if(mapView.current){
+            mapView.current.animateToRegion(newRegion)
+          }
+        })
+    }
+    startListener()
+    return () => {
+      if(locationSubscription.current){
+        locationSubscription.current.remove() //Turns off listener if it exists
+      }
+    }
+  }, [])
+
+  async function openModal() {
+    //Fetch data from weather api
+
+    
+
+    setModalVisible(true);
+  };
+  
+  function closeModal() {
+    setModalVisible(false);
+    saveImage();
+    setImagePath('')
+  };
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView 
+      style={styles.map}
+      region={region}
+      onLongPress={getWeather}
+      ref = {mapView}></MapView>
+    <PopupModal visible={modalVisible} onClose={closeModal}/>
+
     </View>
   );
 }
 
+
+const PopupModal = ({visible, onClose}) => {
+
+  
+  return (
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={visible}
+      onRequestClose={onClose}
+    >
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
+        <View style={{ backgroundColor: 'white', padding: 20, borderRadius: 10 }}>
+          <Text>Add image to this location</Text>
+          <Button title="Add image" onPress={openImageSelector}/>
+          <Button title="Close" onPress={onClose} />
+          <View style={{ position: 'relative' }}>
+            {imagePath && <Image source={{ uri: imagePath }} style={styles.locationImage} />}
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1,  
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
